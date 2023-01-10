7de4d7394e230c7408c8ac73799d611a544a12bf
Clemens Hübner <clemens.huebner@innogames.com>
2023-01-10 16:53:39 +0100

    added scripts

--- src_typescript/xitem_ui_server.ts
+++ src_typescript/xitem_ui_server.ts
@@ -2,7 +2,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import ui from "./UI";
-import { Server } from 'http';
 const PORT = process.env.PORT || 3000;
 
-const httpServer: Server = ui.listen(PORT, () => console.log(process.env.APP_NAME + " UI started on Port: " + PORT + "!"));
+ui.listen(PORT, () => console.log("Xitem Website started on Port: " + PORT + "!"));
